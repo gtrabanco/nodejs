f3b2b613fbf246dcb7dd2fba972966d506676e00
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2015-09-15 21:40:47 +0430

    Added a change

--- 01_servidor_basico/index.js
+++ 01_servidor_basico/index.js
@@ -1,4 +1,13 @@
 /* Created by Gabriel Trabanco <gtrabanco@fwok.org> */
-
 "use strict";
 
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {'Content-type': 'text/plain'});
+    response.end('Wake up, Neo... But wait I added this wait.. Would you bring me the change nodemon?\n');
+});
+
+server.listen(8080, '127.0.0.1');
+console.log('Server started');
