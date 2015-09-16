343a0a6915d4921af21abe07c29b4f02a33ab96a
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2015-09-16 20:03:43 +0430

    Example of async where you can watch the flow of the app.

---
+++ 02_functions_async/async_function.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("The beginning of the application.\n");
+
+function write_after_2seconds(msg, callBack) {
+    setTimeout(function(){
+        console.log(msg);
+        callBack(msg);
+    }, 2000);
+}
+
+
+write_after_2seconds("Two senconds delay.\n", function(msg){
+    console.log("I have written " + msg + "\n");
+});
+
+console.log("The finish of the application.\n");
