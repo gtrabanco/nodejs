d6f35a937a8a67dea4babc816f81b770332e321b
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2015-09-20 22:28:28 +0430

    Failed testing with routes in express generator

---
+++ nodeapi/server/routes/users.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+
+//We can also pass vars in body but get does not have body because it is only for petitions
+
+
+
+//This will response / or /?var=foo (...)
+router.get('/', function(req, res, next) {
+    res.send('Respond to the main');
+    console.log('The query string', req.query);
+});
+
+
+//Because the previous route this wont response to / but it could if it where firts
+//And to a param like /7 or /ashfjkjs
+router.get('/(:id)?', function(req, res, next) {
+    res.send('Respond to a var');
+    var id = parseInt(req.param.id);
+
+    //If we didn't receive a valid id, we dont need to process all, do we?
+    if (id > 0) {
+        console.log('The requested user was %d', req.params.id);
+
+
+        //And maybe it would be a great idea use query strings for filters like age, for example
+        if (req.query.length > 0) {
+            //The if is not neccessary it just would return undefined or nan to the vars
+            //just make yourself sure you are receiveing what you want
+            console.log("Query string received: ", req.query);
+            var ageFilter = parseInt(req.query.age);
+            console.log("The age filter for %d is %s", req.params.id, (ageFilter > 0? ageFilter:"No valid age"));
+        }
+    }
+});
+
+router.put('/p', function(req, res, next){
+    console.log('body', req.body);
+    res.send("Responding to a post.");
+});
+
+module.exports = router;
